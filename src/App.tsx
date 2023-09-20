4e50e083f41d215ef6f1010948baf067494a70b4
unknown <ieva.vyliaudaite@me.com>
2023-09-20 10:09:15 +0300

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { FC } from 'react';
 
-const App = () => {
+const App: FC = () => {
   return (
     <div>App</div>
   )
 }
 
-export default App
+export default App;
